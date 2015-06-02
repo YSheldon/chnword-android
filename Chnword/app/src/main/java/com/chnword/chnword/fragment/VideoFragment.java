28986494c7aad43ab41f442977e301a80d57a733
魏凯凯 <137067401@qq.com>
2015-06-02 12:12:16 +0800

    视频播放完成

--- Chnword/app/src/main/java/com/chnword/chnword/fragment/VideoFragment.java
+++ Chnword/app/src/main/java/com/chnword/chnword/fragment/VideoFragment.java
@@ -2,9 +2,14 @@ package com.chnword.chnword.fragment;
 
 import android.app.Fragment;
 import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.MediaController;
@@ -19,41 +24,44 @@ import java.io.IOException;
 /**
  * Created by khtc on 15/5/30.
  */
-public class VideoFragment extends Fragment {
+public class VideoFragment extends Fragment implements MediaPlayer.OnBufferingUpdateListener, MediaPlayer.OnCompletionListener, MediaPlayer.OnPreparedListener,
+        MediaPlayer.OnVideoSizeChangedListener, SurfaceHolder.Callback {
+    private static final String TAG = VideoFragment.class.getSimpleName();
 
     private VideoView videoView;
     private Uri uri;
     private Word word;
 
+
+    private MediaPlayer mMediaPlayer;
+    private SurfaceView mPreview;
+    private SurfaceHolder holder;
+
+    private int mVideoWidth;
+    private int mVideoHeight;
+    private boolean mIsVideoSizeKnown = false;
+    private boolean mIsVideoReadyToBePlayed = false;
+    private String path;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_video, container, false);
 
-        videoView = (VideoView) view.findViewById(R.id.video_view);
-        videoView.setMediaController(new MediaController(this.getActivity()));
-
-//        String path = this.getActivity().getAssets().;
-//        videoView.setVideoURI();
-//        Uri uri = Uri.parse("file:///android_asset/cft.mp4");
-//        videoView.setVideoURI(uri);
+//        videoView = (VideoView) view.findViewById(R.id.video_view);
+//        videoView.setMediaController(new MediaController(this.getActivity()));
+//        videoView.setOnErrorListener(videoErrorListener);
+////        videoView.setVideoURI(Uri.parse("android.resource://com.chnword.chnword/"+R.raw.12237832415));
+//        videoView.setVideoURI(Uri.parse("http://forum.ea3w.com/coll_ea3w/attach/2008_10/12237832415.3gp"));
+//        videoView.requestFocus();
 //        videoView.start();
 
-        try {
-            AssetFileDescriptor descriptor = getActivity().getAssets().openFd("cft.mp4");
-
-            FileDescriptor fileDescriptor = descriptor.getFileDescriptor();
-//            Uri uri1 = Uri.p
-
-//            videoView.set
-            String uri1 = "android.resource://" + this.getActivity().getPackageName() + "/" + "cft.mp4";
 
 
-            videoView.setVideoURI(Uri.parse(uri1));
-            videoView.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        mPreview = (SurfaceView) view.findViewById(R.id.surfaceView);
+//        holder = mPreview.getHolder();
+        holder = mPreview.getHolder();
+        holder.addCallback(this);
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 
 
 
@@ -75,15 +83,6 @@ public class VideoFragment extends Fragment {
         super.onResume();
     }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-    }
 
 
     // setter and getter
@@ -109,5 +108,131 @@ public class VideoFragment extends Fragment {
 
     public void setWord(Word word) {
         this.word = word;
+    }
+
+
+    public MediaPlayer.OnErrorListener videoErrorListener = new MediaPlayer.OnErrorListener()
+    {
+        @Override
+        public boolean onError(MediaPlayer mp, int what, int extra) {
+            Log.e(TAG, what + " " + extra);
+            return false;
+        }
+    };
+
+
+
+
+
+    private void playVideo() {
+        doCleanUp();
+        try {
+            /*
+             * Set path variable to progressive streamable mp4 or 3gpp
+             * format URL. Http protocol should be used. Mediaplayer can only
+             * play "progressive streamable contents" which basically means: 1.
+             * the movie atom has to precede all the media data atoms. 2. The
+             * clip has to be reasonably interleaved.
+             */
+            path = "http://www.androidbook.com/akc/filestorage/android/documentfiles/3389/movie.mp4";
+            path = "http://forum.ea3w.com/coll_ea3w/attach/2008_10/12237832415.3gp";
+
+            // Create a new media player and set the listeners
+            mMediaPlayer = new MediaPlayer();
+            mMediaPlayer.setDataSource(path);
+            mMediaPlayer.setDisplay(holder);
+            mMediaPlayer.prepare();
+            mMediaPlayer.setOnBufferingUpdateListener(this);
+            mMediaPlayer.setOnCompletionListener(this);
+            mMediaPlayer.setOnPreparedListener(this);
+            mMediaPlayer.setOnVideoSizeChangedListener(this);
+            mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+//            mMediaPlayer.start();
+
+        } catch (Exception e) {
+            Log.e(TAG, "error: " + e.getMessage(), e);
+        }
+    }
+
+
+    public void onBufferingUpdate(MediaPlayer arg0, int percent) {
+        Log.d(TAG, "onBufferingUpdate percent:" + percent);
+
+    }
+
+    public void onCompletion(MediaPlayer arg0) {
+        Log.d(TAG, "onCompletion called");
+    }
+
+    public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+        Log.v(TAG, "onVideoSizeChanged called");
+        if (width == 0 || height == 0) {
+            Log.e(TAG, "invalid video width(" + width + ") or height(" + height
+                    + ")");
+            return;
+        }
+        mIsVideoSizeKnown = true;
+        mVideoWidth = width;
+        mVideoHeight = height;
+        if (mIsVideoReadyToBePlayed && mIsVideoSizeKnown) {
+            startVideoPlayback();
+        }
+    }
+
+    public void onPrepared(MediaPlayer mediaplayer) {
+        Log.d(TAG, "onPrepared called");
+        mIsVideoReadyToBePlayed = true;
+        if (mIsVideoReadyToBePlayed && mIsVideoSizeKnown) {
+            startVideoPlayback();
+        }
+    }
+
+    public void surfaceChanged(SurfaceHolder surfaceholder, int i, int j, int k) {
+        Log.d(TAG, "surfaceChanged called");
+
+    }
+
+    public void surfaceDestroyed(SurfaceHolder surfaceholder) {
+        Log.d(TAG, "surfaceDestroyed called");
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        Log.d(TAG, "surfaceCreated called");
+        playVideo();
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        releaseMediaPlayer();
+        doCleanUp();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        releaseMediaPlayer();
+        doCleanUp();
+    }
+
+    private void releaseMediaPlayer() {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
+    }
+
+    private void doCleanUp() {
+        mVideoWidth = 0;
+        mVideoHeight = 0;
+        mIsVideoReadyToBePlayed = false;
+        mIsVideoSizeKnown = false;
+    }
+
+    private void startVideoPlayback() {
+        Log.v(TAG, "startVideoPlayback");
+        holder.setFixedSize(mVideoWidth, mVideoHeight);
+        mMediaPlayer.start();
     }
 }
