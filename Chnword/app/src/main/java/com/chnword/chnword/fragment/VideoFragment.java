eed8518c974567785a11fd276cde1751664139b3
kaikaiwei <137067401@qq.com>
2015-06-02 08:42:05 +0800

    video play uncomplete

--- Chnword/app/src/main/java/com/chnword/chnword/fragment/VideoFragment.java
+++ Chnword/app/src/main/java/com/chnword/chnword/fragment/VideoFragment.java
@@ -1,6 +1,7 @@
 package com.chnword.chnword.fragment;
 
 import android.app.Fragment;
+import android.content.res.AssetFileDescriptor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -11,6 +12,9 @@ import android.widget.VideoView;
 
 import com.chnword.chnword.R;
 import com.chnword.chnword.beans.Word;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
 
 /**
  * Created by khtc on 15/5/30.
@@ -28,8 +32,29 @@ public class VideoFragment extends Fragment {
         videoView = (VideoView) view.findViewById(R.id.video_view);
         videoView.setMediaController(new MediaController(this.getActivity()));
 
-        String path = this.getActivity().getAssets().;
-        videoView.setVideoURI();
+//        String path = this.getActivity().getAssets().;
+//        videoView.setVideoURI();
+//        Uri uri = Uri.parse("file:///android_asset/cft.mp4");
+//        videoView.setVideoURI(uri);
+//        videoView.start();
+
+        try {
+            AssetFileDescriptor descriptor = getActivity().getAssets().openFd("cft.mp4");
+
+            FileDescriptor fileDescriptor = descriptor.getFileDescriptor();
+//            Uri uri1 = Uri.p
+
+//            videoView.set
+            String uri1 = "android.resource://" + this.getActivity().getPackageName() + "/" + "cft.mp4";
+
+
+            videoView.setVideoURI(Uri.parse(uri1));
+            videoView.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
 
 
         return view;
