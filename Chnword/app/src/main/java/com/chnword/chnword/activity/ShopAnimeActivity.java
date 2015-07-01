35078d80d2673b495e564d2ce3c07f2e9921ab10
kaikaiwei <137067401@qq.com>
2015-07-01 08:48:37 +0800

    商城界面

---
+++ Chnword/app/src/main/java/com/chnword/chnword/activity/ShopAnimeActivity.java
@@ -0,0 +1,34 @@
+package com.chnword.chnword.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.chnword.chnword.R;
+
+/**
+ * Created by khtc on 15/7/1.
+ */
+public class ShopAnimeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_anime);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
---
+++ Chnword/app/src/main/java/com/chnword/chnword/activity/ShopSuitActivity.java
@@ -0,0 +1,37 @@
+package com.chnword.chnword.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.chnword.chnword.R;
+
+/**
+ * 家庭版套装
+ * Created by khtc on 15/7/1.
+ */
+public class ShopSuitActivity extends Activity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_suit);
+        // item item_shop_suit
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
