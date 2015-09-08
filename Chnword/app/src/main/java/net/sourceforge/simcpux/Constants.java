3ba2d81352ca72d86c53643e19a3c994ac1ffc49
kaikaiwei <137067401@qq.com>
2015-09-08 08:02:23 +0800

    商户号，秘钥

--- Chnword/app/src/main/java/net/sourceforge/simcpux/Constants.java
+++ Chnword/app/src/main/java/net/sourceforge/simcpux/Constants.java
@@ -11,11 +11,11 @@ public class Constants {
 
 
   //商户号
-   public static final String MCH_ID = "1233848001";
+   public static final String MCH_ID = "1261899501";
 
 
 //  API密钥，在商户平台设置
-    public static final  String API_KEY="412fde4e9c2e2bb619514ecea142e449";
+    public static final  String API_KEY="04y2MoeXcNQXQVr3OypRjmIFJoKKa24d";
 
 
 
