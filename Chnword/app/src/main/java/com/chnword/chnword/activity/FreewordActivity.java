e941b4a65bcc3b7fdbd251dc79707677337e6f96
kaikaiwei <137067401@qq.com>
2015-09-15 22:44:05 +0800

    cate activity added

---
+++ Chnword/app/src/main/java/com/chnword/chnword/activity/FreewordActivity.java
@@ -0,0 +1,42 @@
+package com.chnword.chnword.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.chnword.chnword.R;
+
+/**
+ * Created by khtc on 15/9/15.
+ */
+public class FreewordActivity extends Activity {
+
+    private ImageButton backImageButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        backImageButton = (ImageButton) findViewById(R.id.backImageButton);
+        backImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();//回退
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+}
