788faec9195ae76fd19484f023113e94a8b600ca
kaikaiwei <137067401@qq.com>
2015-09-22 20:06:45 +0800

    网址修改

--- Chnword/app/src/main/java/com/chnword/chnword/net/NetConf.java
+++ Chnword/app/src/main/java/com/chnword/chnword/net/NetConf.java
@@ -5,25 +5,39 @@ package com.chnword.chnword.net;
  */
 public class NetConf
 {
-    public static final String URL_VERIFY  = "http://app.3000zi.com/api/verify.php";
-    public static final String URL_LIST    = "http://app.3000zi.com/api/list.php";
-    public static final String URL_SUBLIST = "http://app.3000zi.com/api/sublist.php";
-    public static final String URL_WORD    = "http://app.3000zi.com/api/word.php";
-    public static final String URL_SHOW    = "http://app.3000zi.com/api/show.php";
     public static final String URL_REGIST  = "http://app.3000zi.com/api/regist.php";
-    public static final String URL_VERSION_CHECK = "http://app.3000zi.com/api/version.php";
-
 
     public static final String URL_MEMBER       = "http://app.3000zi.com/web/member.php";
-    public static final String URL_FEEDBACK     = "http://app.3000zi.com/web/member.php";
+
     public static final String URL_DOWNLOAD     = "http://app.3000zi.com/web/download.php";
     public static final String URL_ABOUT        = "http://app.3000zi.com/web/about.php";
 
     public static final String URL_WXPAY        = "http://app.3000zi.com/api/wx_pay.php";
 
-
     public static final String URL_FAQ  = "http://www.3000zi.com/faq.html";
-
     public static final String URL_BUY = "http://www.baidu.com";
+
+
+
+    //新接口
+    public static final String URL_LOGIN = "http://app.3000zi.com/api/login.php";//登录
+    public static final String URL_VERIFY  = "http://app.3000zi.com/api/verify.php";//激活码验证接口
+    public static final String URL_SEND = "http://app.3000zi.com/api/send.php";//验证码发送接口
+    public static final String URL_BIND = "http://app.3000zi.com/api/bind.php";//绑定接口
+    public static final String URL_LIST    = "http://app.3000zi.com/api/list.php";//分类列表
+    public static final String URL_SUBLIST = "http://app.3000zi.com/api/sublist.php";//汉字接口列表
+    public static final String URL_SHOW    = "http://app.3000zi.com/api/show.php";//汉字展示接口
+    public static final String URL_WORD    = "http://app.3000zi.com/api/word.php";//二维码扫描接口
+    public static final String URL_VERSION_CHECK = "http://app.3000zi.com/api/version.php";//版本检测接口
+    public static final String URL_FEEDBACK     = "http://app.3000zi.com/api/feedback.php";//一件反馈接口
+
+    public static final String URL_FORGET = "http://app.3000zi.com/api/forget.php";//找回密码
+    public static final String URL_SHARED = "http://app.3000zi.com/api/share_word.php";//获取分享的字
+    public static final String URL_SHOPLIST = "http://app.3000zi.com/api/shop_list.php";//商品列表
+
+
+
+
+
 
 }
