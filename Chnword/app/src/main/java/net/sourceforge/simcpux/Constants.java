abaaf2916f4e8e3b355dceb44ef2ff0ec64c0fbb
kaikaiwei <137067401@qq.com>
2015-09-26 10:24:47 +0800

    微信支付完成

--- Chnword/app/src/main/java/net/sourceforge/simcpux/Constants.java
+++ Chnword/app/src/main/java/net/sourceforge/simcpux/Constants.java
@@ -7,12 +7,22 @@ public class Constants {
     //请同时修改  androidmanifest.xml里面，.PayActivityd里的属性<data android:scheme="wxb4ba3c02aa476ea1"/>为新设置的appid
   public static final String APP_ID = "wx523e7fec6968506f";
 
-
-
-
   //商户号
    public static final String MCH_ID = "1261899501";
 
+    /*
+    APPID = 'wx523e7fec6968506f';
+MCHID = '1261899501';
+KEY = '44bc0758e035f0cbeca3b845WangMing';
+APPSECRET = '139c374c294db154f5b6ec0db76ad4df';
+合作者身份(PID)：2088021351529570
+安全校验码(Key)：默认加密： tqnspyb9h1068p34hm6z9lmk479kefc5
+
+ORI
+
+04y2MoeXcNQXQVr3OypRjmIFJoKKa24d
+
+     */
 
 //  API密钥，在商户平台设置
     public static final  String API_KEY="04y2MoeXcNQXQVr3OypRjmIFJoKKa24d";
