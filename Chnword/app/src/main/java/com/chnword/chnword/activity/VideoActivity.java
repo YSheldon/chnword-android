418b8ac0fe887933e0edf005566b245f126565f1
kaikaiwei <137067401@qq.com>
2015-10-27 21:12:57 +0800

    视频播放

--- Chnword/app/src/main/java/com/chnword/chnword/activity/VideoActivity.java
+++ Chnword/app/src/main/java/com/chnword/chnword/activity/VideoActivity.java
@@ -61,4 +61,10 @@ public class VideoActivity extends Activity
     protected void onDestroy() {
         super.onDestroy();
     }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
 }
