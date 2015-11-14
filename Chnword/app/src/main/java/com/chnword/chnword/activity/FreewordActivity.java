8ab721f6c342f34d845fe5c4440d0eb63db0dbf0
kaikaiwei <137067401@qq.com>
2015-11-14 10:29:10 +0800

    分享流程初步修改

--- Chnword/app/src/main/java/com/chnword/chnword/activity/FreewordActivity.java
+++ Chnword/app/src/main/java/com/chnword/chnword/activity/FreewordActivity.java
@@ -18,6 +18,7 @@ import android.widget.Toast;
 
 import com.chnword.chnword.R;
 import com.chnword.chnword.beans.Word;
+import com.chnword.chnword.beans.WordShare;
 import com.chnword.chnword.net.AbstractNet;
 import com.chnword.chnword.net.DeviceUtil;
 import com.chnword.chnword.net.NetConf;
@@ -27,7 +28,13 @@ import com.chnword.chnword.popwindow.SharePopWindow;
 import com.chnword.chnword.store.LocalStore;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.umeng.socialize.bean.SHARE_MEDIA;
+import com.umeng.socialize.bean.SocializeEntity;
+import com.umeng.socialize.bean.StatusCode;
+import com.umeng.socialize.controller.UMServiceFactory;
+import com.umeng.socialize.controller.UMSocialService;
+import com.umeng.socialize.controller.listener.SocializeListeners;
 import com.umeng.socialize.media.UMImage;
+import com.umeng.socialize.media.UMVideo;
 import com.umeng.socialize.sso.QZoneSsoHandler;
 import com.umeng.socialize.sso.SinaSsoHandler;
 import com.umeng.socialize.sso.UMQQSsoHandler;
@@ -104,6 +111,7 @@ public class FreewordActivity extends Activity {
     String url = "";
 
     private TextView freewordCode;
+    private List<WordShare> wordList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -143,6 +151,7 @@ public class FreewordActivity extends Activity {
             finish();
         }
 
+        wordList = new ArrayList<WordShare>();
         //
         modulNameTextView.setText(title);
 
@@ -197,7 +206,7 @@ public class FreewordActivity extends Activity {
         titleTextView.setText(title);
 
 
-        requestNet(zoneCode);
+        requestSharedWord(zoneCode);
 
     }
 
@@ -212,18 +221,106 @@ public class FreewordActivity extends Activity {
         finish();
     }
 
-    private void requestNet(String zoneCode) {
 
+
+    private void requestSharedWord(String zoneCode) {
+        LocalStore store = new LocalStore(this);
         String userid = store.getDefaultUser();
         String deviceId = DeviceUtil.getDeviceId(this);
-        JSONObject param = NetParamFactory.subListParam(userid, deviceId, zoneCode, 0, 0);
-        AbstractNet net = new VerifyNet(handler, param, NetConf.URL_SUBLIST);
+
+        JSONObject param = NetParamFactory.sharedWordParam(userid, deviceId, type);
+        AbstractNet net = new VerifyNet(sharehandler, param, NetConf.URL_SHARED);
         progressDialog = ProgressDialog.show(this, "提示", "loading...");
         net.start();
-
     }
 
-    Handler handler = new Handler(){
+    private UMSocialService mController = UMServiceFactory.getUMSocialService("com.umeng.share");
+    String currentShareWord = "";
+
+    private void setUpSharedContent() {
+        // 设置分享内容
+        mController.setShareContent("123333333");
+        // 设置分享图片, 参数2为图片的url地址
+        mController.setShareMedia(new UMImage(FreewordActivity.this, "123333333"));
+        // 设置分享视频
+        UMVideo umVideo = new UMVideo("123333333");
+        // 设置视频缩略图
+        umVideo.setThumb("123333333");
+        umVideo.setTitle("三千字");
+        mController.setShareMedia(umVideo);
+
+        //添加微信和朋友圈
+        String appId = "wx523e7fec6968506f";
+        String appSecret = "4a01f28bf8671d6b5487094caaffc72e";
+        // 添加微信平台
+        UMWXHandler wxHandler = new UMWXHandler(FreewordActivity.this, appId, appSecret);
+        wxHandler.addToSocialSDK();
+
+        // 添加微信朋友圈
+        UMWXHandler wxCircleHandler = new UMWXHandler(FreewordActivity.this, appId, appSecret);
+        wxCircleHandler.setToCircle(true);
+        wxCircleHandler.addToSocialSDK();
+
+        //添加qq的
+        //参数1为当前Activity，参数2为开发者在QQ互联申请的APP ID，参数3为开发者在QQ互联申请的APP kEY.
+        UMQQSsoHandler qqSsoHandler = new UMQQSsoHandler(FreewordActivity.this, "1104685705", "TaZo5RPmrGX11nPO");
+        qqSsoHandler.addToSocialSDK();
+
+        //qq空间
+        QZoneSsoHandler qZoneSsoHandler = new QZoneSsoHandler(FreewordActivity.this, "100424468", "c7394704798a158208a74ab60104f0ba");
+        qZoneSsoHandler.addToSocialSDK();
+
+        //添加新浪的
+        mController.getConfig().setSsoHandler(new SinaSsoHandler());
+    }
+
+    /**
+     * 分享成功的回调
+     * @param platform
+     */
+    private void performShare(SHARE_MEDIA platform) {
+        mController.postShare(this, platform, new SocializeListeners.SnsPostListener() {
+
+            @Override
+            public void onStart() {
+
+            }
+
+            @Override
+            public void onComplete(SHARE_MEDIA platform, int eCode, SocializeEntity entity) {
+                String showText = platform.toString();
+                if (eCode == StatusCode.ST_CODE_SUCCESSED) {
+                    showText += "平台分享成功";
+
+                    //调用说明成功
+                    boolean flag = false;
+                    for (int i = 0; i < wordList.size(); i ++) {
+                        WordShare wordShare = wordList.get(i);
+                        if (!store.isWordShared(wordShare.getWord())) {
+                            flag = true;
+                        }
+                    }
+                    if (flag) {
+                        //有没分享的字
+                        store.setWordShared(currentShareWord);
+                    } else {
+                        store.unLockNextCategory();
+                    }
+
+                } else {
+                    showText += "平台分享失败";
+
+                }
+//                Toast.makeText(FreewordActivity.this, showText, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+
+
+
+    Handler sharehandler = new Handler(){
 
         @Override
         public void handleMessage(Message msg) {
@@ -232,30 +329,44 @@ public class FreewordActivity extends Activity {
             try {
                 if (msg.what == AbstractNet.NETWHAT_SUCESS)
                 {
-
+                    wordList.clear();
                     Bundle b = msg.getData();
                     String str = b.getString("responseBody");
-                    Log.e(TAG, str);
+                    android.util.Log.e(TAG, str);
                     JSONObject obj = new JSONObject(str);
+                    Log.e(TAG, obj.toString());
                     if (!obj.isNull("data")){
 
+                        Log.e(TAG, obj.getString("data"));
                         JSONArray wordArray = obj.getJSONArray("data");
+                        Log.e(TAG, wordArray.toString());
                         for (int i = 0; i < wordArray.length(); i ++) {
                             JSONObject wordObj = wordArray.getJSONObject(i);
-                            Word word = new Word();
+                            WordShare word = new WordShare();
+
+                            word.setGifUrl(wordObj.getString("gif"));
+                            word.setVideoUrl(wordObj.getString("video"));
+                            word.setIconUrl(wordObj.getString("icon"));
                             word.setWord(wordObj.getString("word"));
-                            word.setWordIndex(wordObj.getString("unicode"));
-
-                            if ("0".equalsIgnoreCase(wordObj.getString("free"))) {
-                                currentWord = word;
-                                url = wordObj.getString("icon");
-                                freewordCode.setText(currentWord.getWord());
-                                break;
-                            }
+                            word.setSort(wordObj.getString("sort"));
+                            word.setShareTitle(wordObj.getString("share_title"));
+                            word.setShareDesc(wordObj.getString("share_desc"));
+                            word.setShareIcon(wordObj.getString("share_icon"));
+                            word.setShareUrl(wordObj.getString("share_url"));
+//                            word.setWordIndex(wordObj.getString("unicode"));
+                            Log.e(TAG, "OBJ:" + wordObj.toString());
+                            Log.e(TAG, word.toString());
+                            wordList.add(word);
                         }
+                        //// TODO: 15/11/14 根据显示的数目进行判断
+                        if (wordList.size() == 1) {
 
-//                        ImageLoader imageLoader = ImageLoader.getInstance();
-//                        imageLoader.displayImage(url, wordImageView);
+                        } else if (wordList.size() == 2) {
+
+                        } else {
+                            //多于两个，错误
+
+                        }
 
                     } else {
                         Toast.makeText(FreewordActivity.this, "服务器无数据返回", Toast.LENGTH_LONG).show();
@@ -324,5 +435,75 @@ public class FreewordActivity extends Activity {
 //        //添加新浪的
 //        mController.getConfig().setSsoHandler(new SinaSsoHandler());
 //    }
+
+
+    //
+
+
+
+    /**
+     * 请求列表
+     * @param zoneCode
+     */
+    private void requestNet(String zoneCode) {
+
+        String userid = store.getDefaultUser();
+        String deviceId = DeviceUtil.getDeviceId(this);
+        JSONObject param = NetParamFactory.subListParam(userid, deviceId, zoneCode, 0, 0);
+        AbstractNet net = new VerifyNet(handler, param, NetConf.URL_SUBLIST);
+        progressDialog = ProgressDialog.show(this, "提示", "loading...");
+        net.start();
+    }
+
+    Handler handler = new Handler(){
+
+        @Override
+        public void handleMessage(Message msg) {
+            progressDialog.dismiss();
+            progressDialog = null;
+            try {
+                if (msg.what == AbstractNet.NETWHAT_SUCESS)
+                {
+
+                    Bundle b = msg.getData();
+                    String str = b.getString("responseBody");
+                    Log.e(TAG, str);
+                    JSONObject obj = new JSONObject(str);
+                    if (!obj.isNull("data")){
+
+                        JSONArray wordArray = obj.getJSONArray("data");
+                        for (int i = 0; i < wordArray.length(); i ++) {
+                            JSONObject wordObj = wordArray.getJSONObject(i);
+                            Word word = new Word();
+                            word.setWord(wordObj.getString("word"));
+                            word.setWordIndex(wordObj.getString("unicode"));
+
+                            if ("0".equalsIgnoreCase(wordObj.getString("free"))) {
+                                currentWord = word;
+                                url = wordObj.getString("icon");
+                                freewordCode.setText(currentWord.getWord());
+                                break;
+                            }
+                        }
+
+//                        ImageLoader imageLoader = ImageLoader.getInstance();
+//                        imageLoader.displayImage(url, wordImageView);
+
+                    } else {
+                        Toast.makeText(FreewordActivity.this, "服务器无数据返回", Toast.LENGTH_LONG).show();
+                    }
+
+                }
+
+                if (msg.what == AbstractNet.NETWHAT_FAIL) {
+
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+
 
 }
