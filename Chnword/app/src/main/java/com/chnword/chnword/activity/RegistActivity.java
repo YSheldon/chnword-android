699a3ec4112336668a26e993cb857a1bdac74854
kaikaiwei <137067401@qq.com>
2015-11-21 19:06:54 +0800

    后台服务启动

--- Chnword/app/src/main/java/com/chnword/chnword/activity/RegistActivity.java
+++ Chnword/app/src/main/java/com/chnword/chnword/activity/RegistActivity.java
@@ -27,6 +27,7 @@ import com.chnword.chnword.net.NetParamFactory;
 import com.chnword.chnword.net.VerifyNet;
 import com.chnword.chnword.popwindow.PopErrorWindow;
 import com.chnword.chnword.popwindow.PopManyWindow;
+import com.chnword.chnword.services.LoginServices;
 import com.chnword.chnword.store.LocalStore;
 
 import org.json.JSONObject;
@@ -58,9 +59,16 @@ public class RegistActivity extends Activity {
         LocalStore store = new LocalStore(this);
         if (!"0".equalsIgnoreCase(store.getDefaultUser()))
         {
-            userLoginOn(store.getDefaultUser());
+            Intent intent = new Intent(this, TabActivity.class);
+            startActivity(intent);
+            Intent service = new Intent(this, LoginServices.class);
+            startService(service);
+
+        } else {
 
         }
+
+
         editText = (EditText) findViewById(R.id.editText);
 
         toManyPopupWindow = new PopManyWindow(this, new View.OnClickListener() {
@@ -122,21 +130,7 @@ public class RegistActivity extends Activity {
     }
 
 
-    public void userLoginOn(String userId) {
-        //默认用户登录接口
 
-        String deviceId = DeviceUtil.getDeviceId(this);
-        JSONObject param = NetParamFactory.loginParam(userId, deviceId);
-
-        AbstractNet net = new VerifyNet(loginHandler, param, NetConf.URL_LOGIN);
-        Log.e(TAG, "USER LOGIN. URL" + NetConf.URL_LOGIN);
-//        progressDialog = ProgressDialog.show(this, "title", "loading");
-        progressDialog = DialogUtil.createLoadingDialog(this, "数据加载中...");
-        net.start();
-        Log.e(TAG, param.toString());
-
-
-    }
 
 
     //提交
@@ -191,11 +185,11 @@ public class RegistActivity extends Activity {
                     int result = obj.getInt("result");
                     if (result == 0 ){
                         //激活码无效
-                        errorPopupWindow.showAtLocation(findViewById(R.id.register_main), Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
+                        errorPopupWindow.showAtLocation(findViewById(R.id.register_main), Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0);
 
                     } else if (result == 2) {
                         //绑定设备太多了
-                        toManyPopupWindow.showAtLocation(findViewById(R.id.register_main), Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
+                        toManyPopupWindow.showAtLocation(findViewById(R.id.register_main), Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0);
 
                     } else if (result == 1) {
                         //正确
@@ -228,48 +222,5 @@ public class RegistActivity extends Activity {
             }
         }
     };
-    Handler loginHandler = new Handler(){
 
-        @Override
-        public void handleMessage(Message msg) {
-            progressDialog.dismiss();
-            try {
-                Bundle b = msg.getData();
-                String str = b.getString("responseBody");
-                Log.e(TAG, "returned code. " + str);
-                if (msg.what == AbstractNet.NETWHAT_SUCESS)
-                {
-                    JSONObject obj = new JSONObject(str);
-                    int result = obj.getInt("result");
-
-
-
-
-                    if (result == 0 ){
-                        //该用户不存在
-                        Toast.makeText(RegistActivity.this, "该用户不存在", Toast.LENGTH_LONG).show();
-
-                    } else if (result == 1) {
-                        //正确
-
-//                        JSONObject data = obj.getJSONObject("data");
-//                        int usertype = data.getInt("type");
-//                        LocalStore store = new LocalStore(RegistActivity.this);
-//                        store.setUserType(usertype);
-
-                        Intent intent = new Intent(RegistActivity.this, TabActivity.class);
-                        startActivity(intent);
-                        finish();
-                    }
-
-                }
-
-                if (msg.what == AbstractNet.NETWHAT_FAIL) {
-                    Toast.makeText(RegistActivity.this, "请检查网络设置。", Toast.LENGTH_LONG).show();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    };
 }
