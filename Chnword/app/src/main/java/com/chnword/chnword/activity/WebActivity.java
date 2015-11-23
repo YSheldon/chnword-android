cd6c2b44edeb5acbd21ff1bcaa0f290c1a4159c1
kaikaiwei <137067401@qq.com>
2015-11-23 21:14:12 +0800

    添加webActivity

---
+++ Chnword/app/src/main/java/com/chnword/chnword/activity/WebActivity.java
@@ -0,0 +1,30 @@
+package com.chnword.chnword.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import com.chnword.chnword.R;
+
+/**
+ * Created by khtc on 15/11/23.
+ */
+public class WebActivity extends Activity {
+    private final static String TAG = WebActivity.class.getSimpleName();
+
+    public static final String URL_KEY = "URL_KEY";
+    private WebView webView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Bundle bundle = getIntent().getExtras();
+        String url = bundle.getString(URL_KEY);
+
+        webView = (WebView) findViewById(R.id.webViewWeb);
+        webView.loadUrl(url);
+
+    }
+}
